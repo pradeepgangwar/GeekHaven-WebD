ee64a72a563ab462c785fd09677dae938fbe183c
pradeepgangwar <pradeepgangwar39@gmail.com>
2017-05-23 14:22:05 +0530

    Added effects to landing page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 /* Landing portview JS*/
 
 $(document).ready(function() {
-				$("#title").hide();
-				$("#arr").hide();
-				$("#title").fadeIn(3500);
-				$("#motto").hide();
-				$("#motto").fadeIn(14000);
-				$("#logo,#one,#two,#three").hide();
-				$("#logo,#one,#two,#three").fadeIn(15000);
 
+				$("#title").hide();
+				$("#motto").hide();
+				$("#arr").hide();
+				$("#logo,#one,#two,#three").hide();
 
 				function bgchange() {
 					$("body").css("background-color", "black");
@@ -16,7 +13,19 @@ $(document).ready(function() {
 				}
 				setInterval(bgchange, 1000);
 
-				$("#arr").fadeIn(14000);
+				
+				function hideele(){
+					$("#title").hide();
+					$("#motto").hide();
+					$("#arr").hide();
+					$("#logo,#one,#two,#three").hide();
+					$("#title").fadeIn(1500,function(){		
+							$("#motto").fadeIn(10000);
+							$("#logo,#one,#two,#three").fadeIn(8000);
+							$("#arr").fadeIn(14000);
+						});
+				}
+				setTimeout(hideele, 1000);					
 
 				function navBar() {
 					$(".nav-wrapper").css("background-color", "black");
