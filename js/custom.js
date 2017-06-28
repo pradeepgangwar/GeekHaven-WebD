cf5934c202b34898af621d5bc0ee8f2fa26d3cfe
pradeepgangwar <pradeepgangwar39@gmail.com>
2017-06-28 13:57:00 +0530

    Added members cards and interactivity

--- js/custom.js
+++ js/custom.js
@@ -26,31 +26,50 @@
       }
     });
 
+    var images = ['https://albanylawtech.files.wordpress.com/2011/03/class.jpg', 'https://www.robosapi.com/assets/images/workshops/web-designing/web-designing-course-details.jpg'
+    , 'http://media.zzpservicedesk.nl/cad7e4c8f165a65b44bdf172210766e1.jpg', 'http://kepran.com/wp-content/uploads/2016/03/Web-Application-Development.jpg'];
+    var random_img = images[Math.floor(Math.random() * images.length)];
+    console.log(random_img);
+    console.log('url("GeekHaven-WebD/img/' + random_img+ '") no-repeat center top');
+
+    $(".banner").css('background', 'url('+random_img+') no-repeat center top');
+
+    var colors = ['#FCD309', '#f6546a', '#0000ff', '#f42a04', '#d0a0ff', '#ff8000', '#FC9100'];
+    var random_color = colors[Math.floor(Math.random() * colors.length)];
+    var random_color1 = colors[Math.floor(Math.random() * colors.length)];
+
+    $('.navbar-default, .navbar-brand, .fea i, .icon-box i').css('color', random_color1);
+
+    $(".text-border").css('border', '5px solid'+ random_color1 +'');
+    $(".text-border1").css('border', '5px solid'+ random_color1 +'');
+    $(".text-border2").css('border', '5px solid'+ random_color1 +'');
+    $(".text-border3").css('border', '5px solid'+ random_color1 +'');
+
     $(".text-border").mouseenter(function(){
-      $(".text-border").css('border', '8px solid #FCD309');
+      $(".text-border").css('border', '8px solid'+ random_color +'');
     });
     $(".text-border").mouseleave(function(){
-      $(".text-border").css('border', '5px solid #5FCF80');
+      $(".text-border").css('border', '5px solid'+ random_color1 +'');
     });
 
     $(".text-border1").mouseenter(function(){
-      $(".text-border1").css('border', '8px solid #FCD309');
+      $(".text-border1").css('border', '8px solid'+ random_color +'');
     });
     $(".text-border1").mouseleave(function(){
-      $(".text-border1").css('border', '5px solid #5FCF80');
+      $(".text-border1").css('border', '5px solid'+ random_color1 +'');
     });
 
     $(".text-border2").mouseenter(function(){
-      $(".text-border2").css('border', '8px solid #FCD309');
+      $(".text-border2").css('border', '8px solid'+ random_color +'');
     });
     $(".text-border2").mouseleave(function(){
-      $(".text-border2").css('border', '5px solid #5FCF80');
+      $(".text-border2").css('border', '5px solid'+ random_color1 +'');
     });
 
     $(".text-border3").mouseenter(function(){
-      $(".text-border3").css('border', '8px solid #FCD309');
+      $(".text-border3").css('border', '8px solid'+ random_color +'');
     });
     $(".text-border3").mouseleave(function(){
-      $(".text-border3").css('border', '5px solid #5FCF80');
+      $(".text-border3").css('border', '5px solid'+ random_color1 +'');
     });
 })(jQuery);
